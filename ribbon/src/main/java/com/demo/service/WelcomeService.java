bc94dc523fd6d172bf95942e8ef00eecdc341c8f
CW <1445422380@qq.com>
2019-03-26 16:43:42 +0800

    springcloudddddd

---
+++ ribbon/src/main/java/com/demo/service/WelcomeService.java
@@ -0,0 +1,16 @@
+package com.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WelcomeService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String welcome(){
+        return restTemplate.getForObject("http://springcloud/say",String.class);
+    }
+}
---
+++ ribbon/src/main/java/com/springcloud/ribbon/RibbonApplication.java
@@ -0,0 +1,28 @@
+package com.springcloud.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan(basePackages = {"com.demo"})
+public class RibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate loadBalanced(){
+		return new RestTemplate();
+	}
+
+
+}
