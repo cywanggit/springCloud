e90a98b888118f89fcc3224d2763e46a95e3027e
CW <1445422380@qq.com>
2019-05-10 09:11:45 +0800

    qqq

--- ribbon/src/main/java/com/demo/controller/WelcomeController.java
+++ ribbon/src/main/java/com/demo/controller/WelcomeController.java
@@ -5,12 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-public class WelcomeController {
+@RequestMapping("/com")
+public class WelcomeController extends BaseController{
     @Autowired
     private WelcomeService welcomeService;
+
+    @Autowired
+    private HttpServletRequest request;
     @RequestMapping("/welcome")
     public String welcome(){
+        String requestURI = request.getRequestURI();
+        String[] split = requestURI.split("/");
+        for (int i = 0; i < split.length; i++) {
+            System.out.println(split[i]);
+        }
+
+
+
         return welcomeService.welcome();
     }
 }
